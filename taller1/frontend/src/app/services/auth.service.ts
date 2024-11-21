f8c39e184c6dadc6a01642850e0781abae122b99
alexoberco <alejandro.bermudez.fajardo@gmail.com>
2024-11-21 11:04:27 -0500

    fix: hidden urls

--- taller1/frontend/src/app/services/auth.service.ts
+++ taller1/frontend/src/app/services/auth.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private apiUrl = 'http://localhost:8080/api/auth';
+  private apiUrl = `${environment.apiUrl}/auth`;
   private tokenKey = 'jwtToken';
 
   constructor(private http: HttpClient) { }
--- taller1/src/main/java/com/web/taller1/controllers/AuthController.java
+++ taller1/src/main/java/com/web/taller1/controllers/AuthController.java
@@ -1,3 +1,4 @@
+// src/main/java/com/web/taller1/controllers/AuthController.java
 package com.web.taller1.controllers;
 
 import java.util.HashMap;
@@ -5,6 +6,7 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value; // Importar @Value
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -18,10 +20,10 @@ import com.web.taller1.repositories.UsuarioRepository;
 import com.web.taller1.security.JwtUtil;
 import com.web.taller1.services.AuthService;
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+
     @Autowired
     private UsuarioRepository usuarioRepository;
 
@@ -31,20 +33,25 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
-    @PostMapping("/login")
-        public ResponseEntity<?> login(@RequestBody Usuario usuario) {
-            Optional<Usuario> user = usuarioRepository.findByEmail(usuario.getEmail());
-            if (user.isPresent() && user.get().getPassword().equals(usuario.getPassword())) {
-                String token = jwtUtil.generateToken(user.get().getEmail());
-                Map<String, String> response = new HashMap<>();
-                response.put("token", token);
-                return ResponseEntity.ok(response);
-            } else {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales inválidas");
-            }
-        }
-
     
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
+    @CrossOrigin(origins = "${frontend.url}")
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Usuario usuario) {
+        Optional<Usuario> user = usuarioRepository.findByEmail(usuario.getEmail());
+        if (user.isPresent() && user.get().getPassword().equals(usuario.getPassword())) {
+            String token = jwtUtil.generateToken(user.get().getEmail());
+            Map<String, String> response = new HashMap<>();
+            response.put("token", token);
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales inválidas");
+        }
+    }
+
+    @CrossOrigin(origins = "${frontend.url}")
     @PostMapping("/register")
     public ResponseEntity<Map<String, String>> register(@RequestBody Usuario usuario) {
         String token = authService.registerUsuario(usuario);
